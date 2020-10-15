eaae0bc313e0ba85ab9f14095d59309182c31399
wojciechkaczmarek <wojciechkaczmarek447@gmail.com>
2020-10-15 14:33:01 +0200

    some additional class

---
+++ src/AdditionalClass.java
@@ -0,0 +1,16 @@
+class AdditionalClass {
+    private String data;
+    private int id;
+
+    public AdditionalClass(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
